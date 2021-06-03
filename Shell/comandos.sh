a88ef2e14e9b7e23728ae4150611a2bfc6936c24
Jhonkz <jaotop37@gmail.com>
2021-06-02 22:26:21 -0300

    Commit Inicial

---
+++ Shell/comandos.sh
@@ -0,0 +1,22 @@
+# Comando	        # Função
+cd	                # Acessa o diretório especificado
+cls                 # Limpa a tela
+copy                # Copia o conteúdo especificado
+xcopy /s            # Copia o conteúdo recursivamente
+date                # Exibe a data
+exit                # Sai do terminal
+find                # Procura por argumentos especificados
+hostname            # Exibe o nome da máquina
+dir                 # Lista o conteúdo do diretório
+md                  # Cria um diretório
+move                # Move arquivos e diretórios
+rename              # Renomeia um arquivo ou diretório
+nslookup            # Realiza uma consulta DNS
+ping                # Testa a conectividade entre hosts
+chdir               # Exibe o diretório atual
+del                 # Deleta algum arquivo
+deltree             # Deleta os diretórios recursivamente
+rd                  # Deleta um diretório
+tree                # Mostra a árvore de diretórios
+ver                 # Verificar versão do sistema operacional
+telnet              # Acessa o cliente telnet
